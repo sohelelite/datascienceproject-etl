5e1b2632d493b82c23c2b941ba2bf1f42c5205cf
Sohel Shaikh <sohel.shaikh@wohlig.com>
2024-11-08 17:17:11 +0530

    Model Training

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import sys
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 
-from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
 
 if __name__ == '__main__' :
     try:
@@ -37,6 +38,13 @@ if __name__ == '__main__' :
         data_transformation_artifact = data_transformation.initiate_data_transformation()
         print(data_transformation_artifact)
         logging.info("data Transformation completed")
+
+        ## Model Training
+        logging.info("Model Training stared")
+        model_trainer_config = ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer = ModelTrainer(model_trainer_config, data_transformation_artifact)
+        model_trainer_artifact = model_trainer.initiate_model_trainer()
+        logging.info("Model Training completed")
 
 
     except Exception as e:
